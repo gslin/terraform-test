b2a5c479a54cecd467c7ef5842f056d810e8bd69
Gea-Suan Lin <gslin@gslin.org>
2019-08-27 14:46:46 +0800

    Add a test vpc and two subnets.

---
+++ vpc.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "gslin-terraform-test"
+    region = "us-east-1"
+    key = "remote/terraform.tfstate"
+    dynamodb_table = "gslin-terraform-test-lock"
+  }
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.10.0.0/16"
+}
+
+resource "aws_subnet" "SN-fixed-1a" {
+  vpc_id = "${aws_vpc.test.id}"
+  cidr_block = "10.10.0.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "SN-fixed-1b" {
+  vpc_id = "${aws_vpc.test.id}"
+  cidr_block = "10.10.1.0/24"
+  availability_zone = "us-east-1b"
+}
