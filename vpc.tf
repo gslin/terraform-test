1a1d507b869eff16285bffb5be31264920723140
Gea-Suan Lin <gslin@gslin.org>
2019-08-27 15:14:08 +0800

    Add name tag(s).

--- vpc.tf
+++ vpc.tf
@@ -13,16 +13,25 @@ terraform {
 
 resource "aws_vpc" "test" {
   cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "test"
+  }
 }
 
 resource "aws_subnet" "SN-fixed-1a" {
   vpc_id = "${aws_vpc.test.id}"
   cidr_block = "10.10.0.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "SN-test-fixed-1a"
+  }
 }
 
 resource "aws_subnet" "SN-fixed-1b" {
   vpc_id = "${aws_vpc.test.id}"
   cidr_block = "10.10.1.0/24"
   availability_zone = "us-east-1b"
+  tags = {
+    Name = "SN-test-fixed-1b"
+  }
 }
